5b76bfbac9e4c403bc602aedffce27c7ceea9026
Acrobaticbag3 <kevintom214@gmail.com>
2023-01-27 10:22:07 +0100

    Add project files.

---
+++ Inlämning5_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Inlämning5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Eric", "Timothy", "Kingen", "Gud", "Astaghfrihullah" };
+            Array.Sort(namn);
+            for(int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+
+        }
+    }
+}
